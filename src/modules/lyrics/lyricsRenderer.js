ccc3fe322e2089a34e56262afc870a6844ed721f
Oleksandr <58863601+MeDustyy@users.noreply.github.com>
2026-04-24 13:08:06 +0300

    fix(i18n): incorrect key for the "Refresh Lyrics" button

--- src/modules/lyrics/lyricsRenderer.js
+++ src/modules/lyrics/lyricsRenderer.js
@@ -2835,7 +2835,7 @@ class LyricsPlusRenderer {
       this.reloadButton.id = "lyrics-plus-reload-button";
       this.reloadButton.innerHTML =
         '<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"><path d="M480-192q-120 0-204-84t-84-204q0-120 84-204t204-84q65 0 120.5 27t95.5 72v-99h72v240H528v-72h131q-29-44-76-70t-103-26q-90 0-153 63t-63 153q0 90 63 153t153 63q84 0 144-55.5T693-456h74q-9 112-91 188t-196 76Z"/></svg>';
-      this.reloadButton.title = t("RefreshLyrics") || "Refresh Lyrics";
+      this.reloadButton.title = t("refreshLyrics") || "Refresh Lyrics";
       this.buttonsWrapper.appendChild(this.reloadButton);
 
       this.reloadButton.addEventListener("click", () => {
